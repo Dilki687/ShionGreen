de2293319f7cc09cdcb41e74498646ae505cda6c
Imeshi Perera <imeshiperera18@gmail.com>
2024-12-11 21:33:07 -0800

    finalfrontend

--- frontend/src/screens/OrderForm.js
+++ frontend/src/screens/OrderForm.js
@@ -111,7 +111,7 @@ const OrderForm = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/orders", {
+      const response = await fetch("https://shion-greenbackend.vercel.app/api/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
