4133aaa85d8dfa069779517eaed954ae214ed3e9
Imeshi Perera <imeshiperera18@gmail.com>
2024-12-11 21:02:20 -0800

    finalchanges

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,28 @@ const cors = require('cors');
 
 const dbConfig = require('./db')
 const dotenv = require("dotenv");
-const passport = require("passport");
+// const passport = require("passport");
 const orderRoutes = require('./routes/orderRoute');
 const authRoutes = require("./routes/auth"); 
 
 dotenv.config();
-require("./config/passport");
+// require("./config/passport");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://shiongreen-815l.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(bodyParser.json());
 app.use(express.json()); // Ensure JSON parsing is supported
-app.use(passport.initialize()); // Initialize passport
-
+// app.use(passport.initialize()); 
 
 // Routes
-
 app.use('/api/orders', orderRoutes);  // This ensures /api/orders/:id will work for DELETE
 app.use("/auth", authRoutes);
 
